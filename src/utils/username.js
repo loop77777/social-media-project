eebd2d1e159fc4219a6b94a9a2a7efd34b9f305d
deepanshu <mdeepanshu143@gmail.com>
2022-04-18 17:20:00 +0530

    random userscreate endpoints done
    Signed-off-by: deepanshu <mdeepanshu143@gmail.com>

---
+++ src/utils/username.js
@@ -0,0 +1,33 @@
+const ADJACTIVES = [
+  "overrated",
+  "mushy",
+  "stupid",
+  "classy",
+  "obedient",
+  "glamorous",
+  "toothsome",
+  "curious",
+  "teeny-tiny",
+];
+
+const OBJECTS = [
+  "Shoes",
+  "Stockings",
+  "Sweater",
+  "Scarf",
+  "Wagon",
+  "Thermometer",
+  "Bones",
+  "Mop",
+  "Flower pot",
+];
+
+function genRandomUsername() {
+  const adj = ADJACTIVES[Math.floor(Math.random() * 9)];
+  const obj = OBJECTS[Math.floor(Math.random() * 9)];
+  return `${adj}-${obj}`;
+}
+
+module.exports = {
+  genRandomUsername,
+};
