7acaed980df90d4b63d333f05ca86dce48481d44
deepanshu <mdeepanshu143@gmail.com>
2022-04-20 13:08:27 +0530

    guest user login done
    Signed-off-by: deepanshu <mdeepanshu143@gmail.com>

--- src/public/app/cbsocial-common.js
+++ src/public/app/cbsocial-common.js
@@ -1,3 +1,23 @@
-$(()=> {
-    $('#navbar').load('/navbar.html')
-})
+$(() => {
+  $("#navbar").load("/components/navbar.html", loginIfNeeded);
+  $("#footer").load("/components/footer.html");
+});
+
+function loginIfNeeded() {
+  window.currentUser = window.localStorage.user
+    ? JSON.parse(window.localStorage.user)
+    : null;
+  if (!currentUser) {
+    $.post("/api/users", {}, (user) => {
+      if (user) {
+        console.log("register current user as ", user.username);
+        window.localStorage.user = JSON.stringify(user);
+        currentUser = user;
+        $("#nav-username").text(currentUser.username);
+      }
+    });
+  } else {
+    console.log("resuming session as ", currentUser.username);
+    $("#nav-username").text(currentUser.username);
+  }
+}
