af75aad9f7541e6356c9f8c4873027d59689c1e1
deepanshu <mdeepanshu143@gmail.com>
2022-04-21 12:26:24 +0530

    getting all posts and showing it on homepage
    Signed-off-by: deepanshu <mdeepanshu143@gmail.com>

--- src/public/app/cbsocial-common.js
+++ src/public/app/cbsocial-common.js
@@ -1,6 +1,7 @@
 $(() => {
   $("#navbar").load("/components/navbar.html", loginIfNeeded);
   $("#footer").load("/components/footer.html");
+  $("#content").load("/components/all-posts.html", loadPosts);
 });
 
 function loginIfNeeded() {
@@ -20,4 +21,34 @@ function loginIfNeeded() {
     console.log("resuming session as ", currentUser.username);
     $("#nav-username").text(currentUser.username);
   }
+} //m-3
+
+function loadPosts() {
+  $.get("/api/posts", {}, (posts) => {
+    if (posts) {
+      for (post of posts) {
+        $("#Posts-container").append(
+          $(`
+          <div class="col-4">
+     <div class="card m-3">
+     <div class="card-body">
+         <h5 class="card-title" id="post-title">${post.title}</h5>
+         <h6 class="card-subtitle mb-2 text-muted" id="userId">${
+           post.user.username
+         }</h6>
+         <p class="card-text" id="post-description">${post.body.substr(
+           0,
+           240
+         )}<a href="#">... read more</a> </p>
+         <a href="#" class="card-link">Comment</a>
+         <a href="#" class="card-link">Like</a>
+     </div>
+     </div>    
+     </div> 
+      `)
+        );
+        console.log(post);
+      }
+    }
+  });
 }
